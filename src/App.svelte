f43b188c13e5c367a34d72481a3c900e2807d38f
Andres Campuzano <campuxano88@gmail.com>
2020-05-29 03:05:32 -0500

    Creating components.

---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+
+	import About from './components/About.svelte';
+	import Text from './components/Text.svelte'
+
+	export let name;
+	export let lastName;
+	let svelteLogo = "https://arepa.s3.amazonaws.com/svelte-logo.png";
+</script>
+
+<style>
+
+	:global(body) {
+		/* background-color: #f2eee2; */
+		background-color: #343739;
+		color: #0084f6;
+	}
+
+	:global(:root) {
+		--theme-color: #a75ca7;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	main img {
+		width: 30%;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 300;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<main>
+	<h1>Hello {name} {lastName}!</h1>
+	<About />
+	<Text anotherText="Hola"/>
+	<img src={svelteLogo} alt="Svelte logo" />
+</main>
---
+++ src/components/About.svelte
@@ -0,0 +1,24 @@
+<script>
+  let someText = `Frontend Developer from Home`;
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<style>
+
+	p {
+    color: var(--theme-color);
+  }
+
+  button {
+    margin: 15px;
+  }
+</style>
+
+<div class="About">
+  <p>{someText}</p>
+  <button on:click={handleClick} >➕Click me {count === 0 ? '' : count}</button>
+</div>
