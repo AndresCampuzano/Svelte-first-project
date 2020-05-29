90589e6ef42b8eb20bed117663818568daa5944c
Andres Campuzano <campuxano88@gmail.com>
2020-05-29 09:52:53 -0500

    ButtonCount.

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,7 @@
 	import Skills from './components/Skills.svelte';
 	import Input from './components/Input.svelte';
 	import Photos from './components/Photos.svelte';
+	import ButtonCount from './components/ButtonCount.svelte';
 
 	export let name;
 	export let lastName;
@@ -72,5 +73,6 @@
 	<Skills />
 	<Input />
 	<Photos />
+	<ButtonCount />
 	<img src={svelteLogo} alt="Svelte logo" />
 </main>
